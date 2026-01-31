40c89f622c0216e3b4628774f9a7966230f80f66
Garrison Osteen <garosteen@gmail.com>
2026-01-31 14:37:54 -0700

    Add detection areas for Door and Computer areas

--- scripts/overworld/overworld.gd
+++ scripts/overworld/overworld.gd
@@ -5,7 +5,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$WindowDetectionArea.body_entered.connect(_on_window_entered)
-
+	$ComputerDetectionArea.body_entered.connect(_on_computer_entered)
+	$DoorDetectionArea.body_entered.connect(_on_door_entered)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,3 +14,9 @@ func _process(delta: float) -> void:
 
 func _on_window_entered(body):
 	print("Window area entered")
+
+func _on_computer_entered(body):
+	print("Computer area entered")
+	
+func _on_door_entered(body):
+	print("Door area entered")
