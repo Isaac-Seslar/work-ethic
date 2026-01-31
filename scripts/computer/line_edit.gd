c365171a464fc8305f999b17ed20259c5afa6323
Isaac Seslar <isaaciscool19@gmail.com>
2026-01-31 14:59:16 -0700

    Added message appending for whos talking

--- scripts/computer/line_edit.gd
+++ scripts/computer/line_edit.gd
@@ -3,22 +3,32 @@ extends LineEdit
 @onready var history_log = $"../MessageBox"
 
 var boss_message_sent:bool = false
+var prepended_val:String=""
 
 func _ready():
+	
 	text_submitted.connect(_on_text_submitted)
 	grab_focus() # Auto-select the typing bar
 
 func _on_text_submitted(new_text):
+	var substring = "Boss:"
+	
+	if substring in new_text:
+		print("boss")
+		_message_formater(new_text)
+	else:
+		print("me")
+		prepended_val="Me: "
+		_message_formater(new_text, prepended_val)
+	
+func _message_formater(new_text, prepended_val: String ="" ):
 	if new_text.strip_edges() == "":
 		return
 	
-	# Add user text to the log
-	if not boss_message_sent:
-		history_log.horizontal_alignment=2
-	else:
-		history_log.horizontal_alignment=0
-		
-	history_log.append_text("\n" + new_text)
+	history_log.horizontal_alignment=0
+	# Add user text to the log	
+	
+	history_log.append_text("\n" + prepended_val + new_text)
 	clear()
 	
 func boss_messages():
@@ -26,16 +36,17 @@ func boss_messages():
 	var boss_message = null
 	
 	match message_picker:
-		1: boss_message = "What are you doing?"
-		2: boss_message = "Whats the status of your deliverables??"
-		3: boss_message = "Lets circle back on that..."
-		4: boss_message = "Understood. I need the ETA on the write up"
-		5: boss_message = "Can you hop on a meeting real quick actually."
+		1: boss_message = "Boss: What are you doing?"
+		2: boss_message = "Boss: Whats the status of your deliverables??"
+		3: boss_message = "Boss: Lets circle back on that..."
+		4: boss_message = "Boss: Understood. I need the ETA on the write up"
+		5: boss_message = "Boss: Can you hop on a meeting real quick actually."
 
 	if text.is_empty():
 		_on_text_submitted(boss_message)
 		boss_message_sent=true
 	else:
 		pass
+		
 	
 	
