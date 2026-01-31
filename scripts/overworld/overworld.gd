7ae65ac777e16287c4335f4ec5a70f137db96f83
Garrison Osteen <garosteen@gmail.com>
2026-01-31 14:52:57 -0700

    Add areas enum and change Overworld state based on which area is entered/exited

--- scripts/overworld/overworld.gd
+++ scripts/overworld/overworld.gd
@@ -1,22 +1,36 @@
 extends Node2D
 
-
+var current_area = Constants.areas.ROOM
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$WindowDetectionArea.body_entered.connect(_on_window_entered)
+	$WindowDetectionArea.body_exited.connect(_on_room_entered)
 	$ComputerDetectionArea.body_entered.connect(_on_computer_entered)
+	$ComputerDetectionArea.body_exited.connect(_on_room_entered)
 	$DoorDetectionArea.body_entered.connect(_on_door_entered)
+	$DoorDetectionArea.body_exited.connect(_on_room_entered)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _on_window_entered(body):
+	current_area = Constants.areas.WINDOW
 	print("Window area entered")
+	print(current_area)
 
 func _on_computer_entered(body):
+	current_area = Constants.areas.COMPUTER
 	print("Computer area entered")
+	print(current_area)
 	
 func _on_door_entered(body):
+	current_area = Constants.areas.DOOR
 	print("Door area entered")
+	print(current_area)
+
+func _on_room_entered(body):
+	current_area = Constants.areas.ROOM
+	print("Room area entered")
+	print(current_area)	
