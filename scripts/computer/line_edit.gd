bb91cec9e0f3971a51cd97fba3caf40504808dc2
Isaac Seslar <isaaciscool19@gmail.com>
2026-01-31 15:04:47 -0700

    Boss introduction added

--- scripts/computer/line_edit.gd
+++ scripts/computer/line_edit.gd
@@ -4,6 +4,7 @@ extends LineEdit
 
 var boss_message_sent:bool = false
 var prepended_val:String=""
+var boss_intoduction_msg:bool = false
 
 func _ready():
 	
@@ -35,6 +36,7 @@ func boss_messages():
 	var message_picker = randi_range(1,5)
 	var boss_message = null
 	
+	
 	match message_picker:
 		1: boss_message = "Boss: What are you doing?"
 		2: boss_message = "Boss: Whats the status of your deliverables??"
@@ -43,8 +45,12 @@ func boss_messages():
 		5: boss_message = "Boss: Can you hop on a meeting real quick actually."
 
 	if text.is_empty():
-		_on_text_submitted(boss_message)
-		boss_message_sent=true
+		if boss_intoduction_msg == false:
+			_on_text_submitted("Boss: Hey how are you?")
+			boss_intoduction_msg=true
+		else:
+			_on_text_submitted(boss_message)
+			boss_message_sent=true
 	else:
 		pass
 		
