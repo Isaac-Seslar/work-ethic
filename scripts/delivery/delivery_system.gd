4de52554080264a3ae402645fc15620b7a149140
Isaac Seslar <isaaciscool19@gmail.com>
2026-01-31 20:28:08 -0700

    Deliver system sends signal

--- scripts/delivery/delivery_system.gd
+++ scripts/delivery/delivery_system.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var cooldown_timer: Timer
-signal cooldown_finished 
+var purchase_started: bool = false
+var pending_delivery_data: StickerData = null
 
 func _ready() -> void:
 	cooldown_timer = Timer.new()
@@ -12,16 +13,31 @@ func _ready() -> void:
 
 	get_node("/root/EventBus").sticker_purchased.connect(_on_purchase_signal_received)
 
-func _on_purchase_signal_received(received_sticker_data: StickerData):
+func _process(delta: float) -> void:
+	if cooldown_timer.is_stopped() and purchase_started:
+		deliver_sticker(pending_delivery_data)
+		purchase_started=false
+		pending_delivery_data = null
+		
+	else:
+		pass
+
+func _on_purchase_signal_received(received_sticker_data: StickerData) -> void:
 	print(received_sticker_data)
 	start_cooldown(Constants.DELIVERY_WAIT_TIME)
-
-func start_cooldown(seconds: float):
+	pending_delivery_data = received_sticker_data
+	purchase_started=true
+	
+	
+func deliver_sticker(recieved_sticker_data) -> void:
+	EventBus.emit_signal("delivery_arrived")
+	
+func start_cooldown(seconds: float) -> void:
 	cooldown_timer.start(seconds)
-
-func cancel_cooldown():
+	
+func cancel_cooldown() -> void:
 	cooldown_timer.stop()
 
 func _on_cooldown_timeout():
 	print("add to delivery inventory")
-	cooldown_finished.emit()
+	
