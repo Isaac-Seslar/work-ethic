e9c85d3b62b0c08afb719e491736287bea5af33f
Isaac Seslar <isaaciscool19@gmail.com>
2026-02-01 13:17:58 -0700

    Hotfix room return button fix

--- scripts/computer/return_to_room_button.gd
+++ scripts/computer/return_to_room_button.gd
@@ -2,4 +2,5 @@ extends Button
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/overworld/overworld.tscn")
+	#get_tree().change_scene_to_file("res://scenes/overworld/overworld.tscn")
+	EventBus.switch_scene.emit(Constants.areas.ROOM)
