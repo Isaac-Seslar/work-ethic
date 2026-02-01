33f970672f64547f6b8670c3cfbfdf856f3caa9a
aanasta <31706287+aanastassatos@users.noreply.github.com>
2026-02-01 13:20:23 -0700

    Added a window scene with the inventory and a show/hide button

---
+++ scripts/window/hide_inventory_button.gd
@@ -0,0 +1,16 @@
+extends Button
+
+@onready var inventory: Panel = $"../../Inventory"
+
+func _ready() -> void:
+	update_text()
+
+func _pressed() -> void:
+	inventory.visible = !inventory.visible
+	update_text()
+
+func update_text() -> void:
+	if inventory.visible:
+		text = "Hide Inventory"
+	else:
+		text = "Show Inventory"
